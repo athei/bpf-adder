21722b6c34e6196c817bb153c532703b343579ef
Alexander Theißen <alex.theissen@me.com>
2022-11-09 14:27:48 +0100

    Use std

--- lib.rs
+++ lib.rs
@@ -1,70 +1,28 @@
-#![no_std]
-#![feature(alloc_error_handler)]
-
 use scale::{Decode, Encode};
-use core::alloc::{
-    GlobalAlloc,
-    Layout,
-};
-use core::cell::RefCell;
-use core::mem::MaybeUninit;
-use core::ptr::{self, NonNull};
-
-#[alloc_error_handler]
-fn oom(_: core::alloc::Layout) -> ! {
-    unsafe { abort() }
-}
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    unsafe { abort() }
-}
-
-
-struct Alloc {
-    heap: RefCell<linked_list_allocator::Heap>,
-}
-
-impl Alloc {
-    const fn new() -> Self {
-        Self {
-            heap: RefCell::new(linked_list_allocator::Heap::empty()),
-        }
-    }
-}
-
-unsafe impl GlobalAlloc for Alloc {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        self.heap
-            .borrow_mut()
-            .allocate_first_fit(layout)
-            .ok()
-            .map_or(ptr::null_mut(), |allocation| allocation.as_ptr())
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        self.heap
-            .borrow_mut()
-            .deallocate(NonNull::new_unchecked(ptr), layout)
-    }
-}
-
-#[global_allocator]
-static mut ALLOCATOR: Alloc = Alloc::new();
-
-pub unsafe fn init() {
-    const HEAP_SIZE: usize = 0x8000;
-    static mut HEAP: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-    ALLOCATOR
-        .heap
-        .borrow_mut()
-        .init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE)
-}
-
 
 extern "C" {
     fn ext_syscall(r1: u64, r2: u64, r3: u64, r4: u64, r5: u64) -> u64;
-    fn abort() -> !;
+}
+
+fn deposit_event(data: &[u8]) {
+    const SYSCALL_NO: u64 = 38;
+    #[derive(Encode)]
+    struct Input {
+        topics_ptr: u64,
+        topics_len: u32,
+        data_ptr: u64,
+        data_len: u32,
+    }
+    let input = Input {
+        topics_ptr: 0,
+        topics_len: 0,
+        data_ptr: data.as_ptr() as usize as _,
+        data_len: data.len() as _,
+    }
+    .encode();
+    unsafe {
+        ext_syscall(SYSCALL_NO, input.as_ptr() as usize as _, 0, 0, 0);
+    }
 }
 
 fn set_storage(key: &[u8], val: &[u8]) -> u32 {
@@ -104,7 +62,9 @@ fn get_storage(key: &[u8], output: &mut [u8]) -> Option<u32> {
     }
     .encode();
     unsafe {
-        if ext_syscall(SYSCALL_NO, input.as_ptr() as usize as _, 0, 0, 0) != 0 {
+        let ret = ext_syscall(SYSCALL_NO, input.as_ptr() as usize as _, 0, 0, 0);
+        deposit_event(ret.to_le_bytes().as_ref());
+        if ret != 0 {
             return None;
         }
     }
@@ -132,10 +92,6 @@ fn input(output: &mut [u8]) -> u32 {
 
 #[no_mangle]
 pub extern "C" fn entrypoint() {
-    unsafe {
-        init()
-    }
-
     let mut buffer = [0u8; 1024];
     let len = input(buffer.as_mut());
 
@@ -143,9 +99,15 @@ pub extern "C" fn entrypoint() {
         return;
     }
 
-    let add = u64::decode(&mut &buffer[..len as usize]).unwrap();
+    let mut input = &buffer[..len as usize];
+    deposit_event(input);
+
+    let add = u64::decode(&mut input).unwrap();
     let old_val = get_storage(&[0], buffer.as_mut())
         .map(|len| u64::decode(&mut &buffer[..len as usize]).unwrap())
         .unwrap_or(0);
+
+    deposit_event(old_val.to_le_bytes().as_ref());
+
     set_storage(&[0], (old_val + add).encode().as_ref());
 }
